bebe4711bd3253e34000358377c304b27cd9732f
abhisheksh99 <abhishekleo1234@gmail.com>
2024-11-08 12:55:03 -0500

    Auth0 Access Token added

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,40 +1,39 @@
 import React from "react";
-import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
-import { useCreateUser } from "@/api/CreateUser";
+import { AppState, Auth0Provider} from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
 
 interface Auth0ProviderWithNavigateProps {
   children: React.ReactNode;
 }
 
-function Auth0ProviderWithNavigate({
-  children,
-}: Auth0ProviderWithNavigateProps) {
-  const { createUser } = useCreateUser();
+const Auth0ProviderWithNavigate: React.FC<Auth0ProviderWithNavigateProps> = ({ children }) => {
+    const navigate = useNavigate();
 
-  const onRedirectCallback = (appState?: AppState, user?: User) => {
-    if (user?.sub && user?.email) {
-      createUser({ auth0Id: user.sub, email: user.email });
+
+    const onRedirectCallback = async (appState?: AppState) => {
+       
+        navigate(appState?.returnTo || "/auth-callback");
+
+    };
+
+    const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+    const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+    const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+
+    if (!domain || !clientId || !redirectUri) {
+        throw new Error("Unable to initialize auth");
     }
-  };
 
-  const domain = import.meta.env.VITE_AUTH0_DOMAIN;
-  const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
-  const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
-
-  if (!domain || !clientId || !redirectUri) {
-    throw new Error("Unable to initialize auth");
-  }
-
-  return (
-    <Auth0Provider
-      domain={domain}
-      clientId={clientId}
-      authorizationParams={{ redirect_uri: redirectUri }}
-      onRedirectCallback={onRedirectCallback}
-    >
-      {children}
-    </Auth0Provider>
-  );
-}
+    return (
+        <Auth0Provider
+            domain={domain}
+            clientId={clientId}
+            authorizationParams={{ redirect_uri: redirectUri }}
+            onRedirectCallback={onRedirectCallback}
+        >
+            {children}
+        </Auth0Provider>
+    );
+};
 
 export default Auth0ProviderWithNavigate;
