a4bffcb916e0bb27cd19ec17d82ca31cd307fdd3
abhisheksh99 <abhishekleo1234@gmail.com>
2024-11-24 12:10:58 -0500

    Navbar Ui created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
 import Login from "./auth/Login";
 import MainLayout from "./layout/MainLayout";
 import Signup from "./auth/Signup";
