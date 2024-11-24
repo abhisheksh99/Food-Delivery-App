4612f94c15e253e95f47724133cd3bdd2773488e
abhisheksh99 <abhishekleo1234@gmail.com>
2024-11-23 23:20:51 -0500

    Verify email page added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import MainLayout from "./layout/MainLayout";
 import Signup from "./auth/Signup";
 import ForgotPassword from "./auth/ForgotPassword";
 import ResetPassword from "./auth/ResetPassword";
+import VerifyEmail from "./auth/VerifyEmail";
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/forgot-password" element={<ForgotPassword/>} />
         <Route path="/reset-password" element={<ResetPassword/>} />
+        <Route path="/verify-email" element={<VerifyEmail/>} />
       </Routes>
     </>
   );
