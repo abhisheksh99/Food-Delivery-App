4347daed8d91bd722c919afe84d54c0bff75eb59
abhisheksh99 <abhishekleo1234@gmail.com>
2024-11-23 21:22:53 -0500

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+import "./App.css"
+
+export default function Home() {
+  return (
+    <div>
+      <Button className="bg-orange hover:bg-hoverOrange">Click me</Button>
+    </div>
+  )
+}
