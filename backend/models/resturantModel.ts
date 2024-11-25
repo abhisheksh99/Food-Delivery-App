377e80ce11d3bd8fe01ad626a58552d441c53487
abhisheksh99 <abhishekleo1234@gmail.com>
2024-11-24 20:11:15 -0500

    Resturant model added

--- backend/models/resturantModel.ts
+++ backend/models/resturantModel.ts
@@ -1,11 +1,57 @@
-import mongoose from "mongoose";
+import mongoose, { Document } from "mongoose";
 
-const resturantSchema = new mongoose.Schema({
+interface IResturant extends Document {
+  user: mongoose.Schema.Types.ObjectId;
+  resturantName: string;
+  city: string;
+  country: string;
+  deliveryTime: number;
+  cuisines: string[];
+  imageUrl: string;
+  menus: mongoose.Schema.Types.ObjectId;
+}
 
-},{
-    timestamps:true
-})
+const resturantSchema = new mongoose.Schema<IResturant>(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    resturantName: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    deliveryTime: {
+      type: Number,
+      required: true,
+    },
+    cuisines: {
+      type: [String],
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+    menus: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Menu",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const Resturant = mongoose.model("Resturant",resturantSchema)
+const Resturant = mongoose.model<IResturant>("Resturant", resturantSchema);
 
-export default Resturant
+export default Resturant;
