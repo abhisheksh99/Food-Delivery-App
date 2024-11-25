b17f101842d005770f79287f3df0dc02ea68cfb8
abhisheksh99 <abhishekleo1234@gmail.com>
2024-11-25 15:10:32 -0500

    Admin, Private Routes added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Login from "./auth/Login";
 import MainLayout from "./layout/MainLayout";
 import Signup from "./auth/Signup";
@@ -14,13 +14,46 @@ import Resturant from "./admin/Resturant";
 import AddMenu from "./admin/AddMenu";
 import Orders from "./admin/Orders";
 import Order from "./components/Order";
+import { useUserStore } from "./store/useUserStore";
 
+
+const ProtectedRoutes = ({ children }: { children: React.ReactNode }) => {
+  const { isAuthenticated, user } = useUserStore();
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+
+  if (!user?.isVerified) {
+    return <Navigate to="/verify-email" replace />;
+  }
+  return children;
+};
+
+const AuthenticatedUser = ({ children }: { children: React.ReactNode }) => {
+  const { isAuthenticated, user } = useUserStore();
+  if(isAuthenticated && user?.isVerified){
+    return <Navigate to="/" replace/>
+  }
+  return children;
+};
+
+const AdminRoute = ({children}:{children:React.ReactNode}) => {
+  const {user, isAuthenticated} = useUserStore();
+  if(!isAuthenticated){
+    return <Navigate to="/login" replace/>
+  }
+  if(!user?.admin){
+    return <Navigate to="/" replace/>
+  }
+
+  return children;
+}
 
 export default function Home() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<MainLayout />}>
+        <Route path="/" element={<ProtectedRoutes><MainLayout /></ProtectedRoutes>}>
           <Route index={true} element={<HeroSection />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/search/:id" element={<SearchPage/>} />
@@ -30,17 +63,17 @@ export default function Home() {
 
 
           {/* Admin */}
-          <Route path="/admin/restaurant" element={<Resturant/>} />
-          <Route path="/admin/menu" element={<AddMenu/>} />
-          <Route path="/admin/orders" element={<Orders/>} />
+          <Route path="/admin/restaurant" element={<AdminRoute><Resturant/></AdminRoute>} />
+          <Route path="/admin/menu" element={<AdminRoute><AddMenu/></AdminRoute>} />
+          <Route path="/admin/orders" element={<AdminRoute><Orders/></AdminRoute>} />
 
         </Route>
 
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-        <Route path="/verify-email" element={<VerifyEmail />} />
+        <Route path="/login" element={<AuthenticatedUser><Login /></AuthenticatedUser>} />
+        <Route path="/signup" element={<AuthenticatedUser><Signup /></AuthenticatedUser>} />
+        <Route path="/forgot-password" element={<AuthenticatedUser><ForgotPassword /></AuthenticatedUser>} />
+        <Route path="/reset-password" element={<AuthenticatedUser><ResetPassword /></AuthenticatedUser>} />
+        <Route path="/verify-email" element={<AuthenticatedUser><VerifyEmail /></AuthenticatedUser>} />
       </Routes>
     </>
   );
