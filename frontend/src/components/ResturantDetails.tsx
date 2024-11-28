807e6391617924ec06ca33b46d185d526ba23eac
abhisheksh99 <abhishekleo1234@gmail.com>
2024-11-27 22:51:28 -0500

    Resturant details page completed

--- frontend/src/components/ResturantDetails.tsx
+++ frontend/src/components/ResturantDetails.tsx
@@ -1,27 +1,34 @@
-import { Timer } from "lucide-react";
+import { useRestaurantStore } from "@/store/useRestaurantStore";
 import AvailableMenu from "./AvailableMenu";
 import { Badge } from "./ui/badge";
+import { Timer } from "lucide-react";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
+const RestaurantDetail = () => {
+  const params = useParams();
+  const { singleRestaurant, getSingleRestaurant } = useRestaurantStore();
 
-
-
-const ResturantDetails = () => {
+  useEffect(() => {
+    getSingleRestaurant(params.id!); 
+    
+  }, [params.id]);
 
   return (
     <div className="max-w-6xl mx-auto my-10">
       <div className="w-full">
         <div className="relative w-full h-32 md:h-64 lg:h-72">
           <img
-            src={ "Loading..."}
+            src={singleRestaurant?.imageUrl || "Loading..."}
             alt="res_image"
             className="object-cover w-full h-full rounded-lg shadow-lg"
           />
         </div>
         <div className="flex flex-col md:flex-row justify-between">
           <div className="my-5">
-            <h1 className="font-medium text-xl">Tandoori tadka</h1>
+            <h1 className="font-medium text-xl">{singleRestaurant?.restaurantName || "Loading..."}</h1>
             <div className="flex gap-2 my-2">
-              {["indian","chinese"].map((cuisine: string, idx: number) => (
+              {singleRestaurant?.cuisines.map((cuisine: string, idx: number) => (
                 <Badge key={idx}>{cuisine}</Badge>
               ))}
             </div>
@@ -29,16 +36,16 @@ const ResturantDetails = () => {
               <div className="flex items-center gap-2">
                 <Timer className="w-5 h-5" />
                 <h1 className="flex items-center gap-2 font-medium">
-                  Delivery Time: <span className="text-[#D19254]">{ "NA"} mins</span>
+                  Delivery Time: <span className="text-[#D19254]">{singleRestaurant?.deliveryTime || "NA"} mins</span>
                 </h1>
               </div>
             </div>
           </div>
         </div>
-       <AvailableMenu />
+       {singleRestaurant?.menus && <AvailableMenu menus = {singleRestaurant?.menus!}/>} 
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ResturantDetails
+export default RestaurantDetail;
